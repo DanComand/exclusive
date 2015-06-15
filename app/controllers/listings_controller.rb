7ebf77732580fad3033f50534f323e7bea4960a1
Dan Comand <dcomand@gmail.com>
2015-06-15 12:20:37 -0400

    generating listing controller, views,and forms.

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,55 @@
+class ListingsController < ApplicationController
+  def index
+  	@listings = Listing.all
+  end
+
+  def show
+  	@listing = Listing.find(params[:id])
+  end
+
+  def new
+  	@listing = Listing.new
+  end
+
+  def edit
+  	@listing = Listing.find(params[:id])
+  end
+
+  def create
+  	@listing = Listing.new(listing_params)
+
+  	if @listing.save
+  		redirect_to listings_url
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	@listing = Listing.find(params[:id])
+
+  	if @listing.update_attributes(listing_params)
+  		redirect_to listing_path(@listing)
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@listing = Listing.find(params[:id])
+  	@listing.destroy
+  	redirect_to listings_path
+  end
+
+  private
+  def product_params
+  	params.require(:product).permit(
+  		:address, 
+  		:city, 
+  		:bedrooms, 
+  		:bathrooms, 
+  		:squarefeet, 
+  		:description)
+  end
+
+end
